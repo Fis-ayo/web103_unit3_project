f3d97e92db2526cbc32691e28a7c8c30e994cfd2
Mercy Akinyemi <akinyemimercy293@gmail.com>
2026-03-14 17:35:59 -0500

    feat: Implement dynamic location routing, add a dedicated events page, and integrate new API services for fetching location and event data.

---
+++ server/data/virtualSpace.js
@@ -0,0 +1,74 @@
+const locationsData = [
+    {
+        name: "Neon Arcade",
+        address: "123 Pixel Blvd",
+        city: "Cyber City",
+        state: "CA",
+        zip: "90210",
+        image: "https://images.unsplash.com/photo-1552820728-8b83bb6b773f?w=600&q=80"
+    },
+    {
+        name: "The Grand Library",
+        address: "456 Knowledge Way",
+        city: "Scroll Town",
+        state: "NY",
+        zip: "10001",
+        image: "https://images.unsplash.com/photo-1507842217343-583bb7270b66?w=600&q=80"
+    },
+    {
+        name: "Echo Arena",
+        address: "789 Soundwave St",
+        city: "Bassville",
+        state: "TX",
+        zip: "73301",
+        image: "https://images.unsplash.com/photo-1540039155733-d71efd54f153?w=600&q=80"
+    },
+    {
+        name: "Starlight Pavilion",
+        address: "101 Nebula Rd",
+        city: "Cosmos",
+        state: "FL",
+        zip: "33101",
+        image: "https://images.unsplash.com/photo-1519681393784-d120267933ba?w=600&q=80"
+    }
+]
+
+const eventsData = [
+    {
+        title: "Retro Gaming Night",
+        date: "2026-10-15",
+        time: "19:00:00",
+        image: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?w=500&q=80",
+        locationID: 1
+    },
+    {
+        title: "Fighting Game Tournament",
+        date: "2027-11-02",
+        time: "14:00:00",
+        image: "https://images.unsplash.com/photo-1511512578047-dfb367046420?w=500&q=80",
+        locationID: 1
+    },
+    {
+        title: "Mystery Book Club",
+        date: "2025-09-28",
+        time: "18:30:00",
+        image: "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?w=500&q=80",
+        locationID: 2
+    },
+    {
+        title: "Indie Rock Festival",
+        date: "2025-12-05",
+        time: "20:00:00",
+        image: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500&q=80",
+        locationID: 3
+    },
+    {
+        title: "Outdoor Movie: Space Jam",
+        date: "2026-10-20",
+        time: "21:00:00",
+        image: "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?w=500&q=80",
+        locationID: 4
+    }
+]
+
+export default { eventsData, locationsData }
