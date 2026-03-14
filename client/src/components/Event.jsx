9c1679d70f1077449f5ed0369e0a5e05a108e4d4
Mercy Akinyemi <akinyemimercy293@gmail.com>
2026-03-14 17:39:15 -0500

    feat: Implement Events page with location filtering, new date utility functions, and a refactored Event component.

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -1,58 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import dates from '../utils/dates'
 import '../css/Event.css'
 
 const Event = (props) => {
-
-    const [event, setEvent] = useState([])
-    const [time, setTime] = useState([])
-    const [remaining, setRemaining] = useState([])
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const eventData = await EventsAPI.getEventsById(props.id)
-                setEvent(eventData)
-            }
-            catch (error) {
-                throw error
-            }
-        }) ()
-    }, [])
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const result = await dates.formatTime(event.time)
-                setTime(result)
-            }
-            catch (error) {
-                throw error
-            }
-        }) ()
-    }, [event])
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const timeRemaining = await dates.formatRemainingTime(event.remaining)
-                setRemaining(timeRemaining)
-                dates.formatNegativeTimeRemaining(remaining, event.id)
-            }
-            catch (error) {
-                throw error
-            }
-        }) ()
-    }, [event])
+    const formattedTime = props.time ? dates.formatTime(props.time) : ''
+    const formattedDate = props.date ? dates.formatDate(props.date) : ''
+    const remainingTime = dates.formatRemainingTime(props.date, props.time)
 
     return (
         <article className='event-information'>
-            <img src={event.image} />
+            <img src={props.image} />
 
             <div className='event-information-overlay'>
                 <div className='text'>
-                    <h3>{event.title}</h3>
-                    <p><i className="fa-regular fa-calendar fa-bounce"></i> {event.date} <br /> {time}</p>
-                    <p id={`remaining-${event.id}`}>{remaining}</p>
+                    <h3>{props.title}</h3>
+                    <p><i className="fa-regular fa-calendar fa-bounce"></i> {formattedDate} <br /> {formattedTime}</p>
+                    {remainingTime === "Event has already passed" ? (
+                        <p className='negative-time-remaining'>{remainingTime}</p>
+                    ) : (
+                        <p id={`remaining-${props.id}`}>{remainingTime}</p>
+                    )}
                 </div>
             </div>
         </article>
